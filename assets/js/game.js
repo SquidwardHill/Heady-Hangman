5e49d6328de501cfd2490b0c1cf52ee70c796494
Sydney <shill@johngroup.com>
2018-06-14 19:36:03 -0400

    need to go back and fix some bugs still

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,31 +1,61 @@
-//declare vars
-var $placeholder = document.getElementById("placeholder");
-var $guessesRemaining = document.getElementById("guessesRemaining");
-var $guessedLetters = document.getElementById("guessedLetters");
+//DOM vars
+var $startGameBtn = document.getElementById("startGame");
+var $placeholderStr = document.getElementById("placeholder");
+//counter
+var $guessesRemainingNum = document.getElementById("guessesRemaining");
+//shows incorrect letter guesses
+var $guessedLettersStr = document.getElementById("guessedLetters");
 var $wins = document.getElementById("wins");
-var $test = document.getElementById("test");
+var $losses = document.getElementById("losses");
 
 
 
-function headyGame() {
-    var wins = 0;
-    var guessesRemaining = 8;
-    var guessedLetters = [];
-    var placeholderArr = [];
-    var headyWord = '';
+$startGameBtn.addEventListener('click', startGame);
 
-    
-    //create an array of variables
-    var headyWordsArr = ['crystals', 'patchoulie', 'natural', 'dreads', 'flowers', 'festivals'];
-    
-    headyWord = headyWordsArr[Math.floor(Math.random() * headyWordsArr.length)];
-    console.log(headyWord + headyWord.length);
-    
-    for (var i = 0; i < headyWord.length ; i++) {
-        placeholderArr.push('_');
-        console.log (placeholderArr.join(' '));
+
+function startGame(){
+ //create an array of variables
+ var wordBankArr = ['crystals', 'patchoulie', 'natural', 'dreads', 'flowers', 'festivals'];
+ var placeholderArr = [];
+ var guessedLettersArr = [];
+
+
+ var headyWord = wordBankArr[Math.floor(Math.random() * wordBankArr.length)];
+ console.log(headyWord);
+    for (var i = 0; i < headyWord.length; i++) {
+        console.log(headyWord[i]);
     }
-    $placeholder.innerHTML = placeholderArr.join(' ');
-    
+}
+
+
+function looper(loop){
+    for (var i = 0; i < loop.length; i++) {
+        console.log(loop[i]);
+    }
+}
+
+
+function makeLowerCase(convert){
+    convert.toLowercase;
 };
-headyGame();
+
+
+function keypressTracker(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
